1bc4f456571bf207ce7922e8eb2e35454cbc3379
Cld.S <shaoyun.claude@gmail.com>
2016-09-30 00:05:40 -0400

    removed duplicated directory of project

--- Birthday_Management_System_lol/src/com/transdyn/dynac/member/MemberDAO.java
+++
@@ -1,5 +0,0 @@
-package com.transdyn.dynac.member;
-
-public interface MemberDAO {
-	public abstract void saveMember();
-}
--- Birthday_Management_System_lol/src/com/transdyn/dynac/member/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.transdyn.dynac.member;
-
-public class MemberService {
-	public void add(Member member)
-	{
-		System.out.println("member added");
-	}
-}
